4f3a5a1c4ccb104c4ec2d3ec53f7656e48567d79
AmirF <faizov.amir@list.ru>
2022-11-27 22:45:26 +0300

    homework6

--- app/src/main/java/com/example/homeworkapp/recycler/adapter/CitiesHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.example.homeworkapp.recycler.adapter
-
-import androidx.recyclerview.widget.RecyclerView
-import com.example.homeworkapp.databinding.ItemCityBinding
-
-class CitiesHolder(binding: ItemCityBinding): RecyclerView.ViewHolder(binding.root) {
-    val idtxt = binding.itemCitiesTextId
-    val nametxt = binding.itemCitiesTextName
-}
