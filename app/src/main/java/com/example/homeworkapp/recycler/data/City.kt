9240f58369281ede28a8e395e3f3dfcc60a0ebfa
AmirF <faizov.amir@list.ru>
2022-12-14 23:58:16 +0300

    homework8

--- app/src/main/java/com/example/homeworkapp/recycler/data/City.kt
+++
@@ -1,6 +0,0 @@
-package com.example.homeworkapp.recycler.data
-
-data class City(
-    val id: Int,
-    val name: String,
-)
--- app/src/main/java/com/example/homeworkapp/recycler/sort/SortingMode.kt
+++
@@ -1,8 +0,0 @@
-package com.example.homeworkapp.recycler.sort
-
-enum class SortingMode {
-    BY_ID_ASC,
-    BY_ID_DESC,
-    BY_NAME_ASC,
-    BY_NAME_DESC,
-}
